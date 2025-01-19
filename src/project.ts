af00ef7bfb14b3fdfdf8b4049556d0796bc5fe1f
Václav Volhejn <vaclav.volhejn@gmail.com>
2025-01-19 19:48:35 +0100

    Rename to np_to_coloring for consistency

--- src/project.ts
+++ src/project.ts
@@ -6,7 +6,7 @@ import commitments_1 from './scenes/commitments_1?scene';
 import commitments_2 from './scenes/commitments_2?scene';
 import discussion1 from './scenes/discussion1?scene';
 import lock from './scenes/lock?scene';
-import mario from './scenes/mario?scene';
+import np_to_coloring from './scenes/np_to_coloring?scene';
 import phone_trust from './scenes/phone_trust?scene';
 import phone from './scenes/phone?scene';
 import rotation from './scenes/rotation?scene';
@@ -31,7 +31,7 @@ export default makeProject({
     lock,
     phone,
     teacher,
-    mario,
+    np_to_coloring,
     phone_trust,
   ],
 });
