e73c0d3aa489a0d2325e49b7d9af64e669aaf9c3
Václav Volhejn <vaclav.volhejn@gmail.com>
2025-01-19 21:21:04 +0100

    Add np_to_coloring_algorithm

--- src/project.ts
+++ src/project.ts
@@ -6,6 +6,7 @@ import commitments_1 from './scenes/commitments_1?scene';
 import commitments_2 from './scenes/commitments_2?scene';
 import discussion1 from './scenes/discussion1?scene';
 import lock from './scenes/lock?scene';
+import np_to_coloring_algorithm from './scenes/np_to_coloring_algorithm?scene';
 import np_to_coloring from './scenes/np_to_coloring?scene';
 import phone_trust from './scenes/phone_trust?scene';
 import phone from './scenes/phone?scene';
@@ -32,6 +33,7 @@ export default makeProject({
     phone,
     teacher,
     np_to_coloring,
+    np_to_coloring_algorithm,
     phone_trust,
   ],
 });
---
+++ src/scenes/np_to_coloring_algorithm.tsx
@@ -0,0 +1,109 @@
+import { Img, makeScene2D, Rect, Txt } from '@motion-canvas/2d';
+import { all, createRef, linear, useLogger, waitFor } from '@motion-canvas/core';
+
+import super_mario_bros_logo_alpha from '../assets/images/super_mario_bros_logo_alpha.png';
+import { Solarized } from '../utilities';
+import { MyTxt } from '../utilities_text';
+
+const inputs = [
+  '→→A→→→←A→→→→→→B→→A→→→←A→→→→←→→B→→→→→A→→→←AA→→→→→→B',
+  'B→→→→→→AA←→→→A→→→→→B→→←→→→→A←→→→A→→B→→→→→→A←→→→A→→',
+];
+
+export default makeScene2D(function* (view) {
+  view.fill(Solarized.base2);
+
+  let image = createRef<Img>();
+  let inputsText = createRef<MyTxt>();
+  let outputTextRect = createRef<Rect>();
+  let outputText1 = createRef<MyTxt>();
+  let outputText2 = createRef<MyTxt>();
+
+  view.add(
+    <>
+      <Rect
+        width={550}
+        height={550}
+        fill={Solarized.orange}
+        zIndex={10}
+        layout
+        direction={'column'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        gap={20}
+      >
+        <Img
+          ref={image}
+          src={super_mario_bros_logo_alpha}
+          width={500}
+          smoothing={true}
+        ></Img>
+        <MyTxt fontSize={100} fill={Solarized.base2}>
+          Algorithm
+        </MyTxt>
+      </Rect>
+      <MyTxt fontSize={100} ref={inputsText}></MyTxt>
+      {/* A rectangle to cover the text in the right half */}
+      <Rect
+        width={2000}
+        x={1000}
+        height={2000}
+        fill={Solarized.base2}
+        zIndex={5}
+      ></Rect>
+      <Rect
+        layout
+        ref={outputTextRect}
+        direction={'column'}
+        width={500}
+        opacity={0}
+        zIndex={7}
+      >
+        <MyTxt
+          fontSize={130}
+          fill={Solarized.red}
+          textAlign={'center'}
+          ref={outputText1}
+        >
+          No
+        </MyTxt>
+        <MyTxt
+          fontSize={60}
+          fill={Solarized.red}
+          textAlign={'center'}
+          ref={outputText2}
+        >
+          World record{'\n'}
+          not broken
+        </MyTxt>
+      </Rect>
+    </>,
+  );
+
+  const logger = useLogger();
+
+  // "No" for the first input, "Yes" for the second input.
+  for (let it = 0; it < 2; it++) {
+    inputsText().text(inputs[it]);
+    outputTextRect().x(100);
+
+    outputText1().text(['No', 'Yes'][it]);
+    outputText2().text(['World record\nnot broken', 'World record\nbroken'][it]);
+    outputText1().fill([Solarized.red, Solarized.green][it]);
+    outputText2().fill([Solarized.red, Solarized.green][it]);
+
+    inputsText().x(-inputsText().width());
+
+    yield* inputsText().x(inputsText().width() / 2, 4, linear);
+
+    yield* all(
+      outputTextRect().opacity(1, 1),
+      outputTextRect().position.add([450, 0], 1),
+    );
+
+    yield* waitFor(1);
+    yield* outputTextRect().opacity(0, 1);
+  }
+
+  yield* waitFor(5);
+});
