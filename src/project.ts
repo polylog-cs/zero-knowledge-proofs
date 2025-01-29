4ad624b5b62bc420bd0a69d309121c34e1aac104
Václav Volhejn <vaclav.volhejn@gmail.com>
2025-01-29 10:11:43 +0100

    Rename discussion scene imports for consistency

--- src/project.ts
+++ src/project.ts
@@ -4,9 +4,9 @@ import building_the_protocol_1 from './scenes/building_the_protocol_1?scene';
 import building_the_protocol_2 from './scenes/building_the_protocol_2?scene';
 import commitments_1 from './scenes/commitments_1?scene';
 import commitments_2 from './scenes/commitments_2?scene';
-import discussion1 from './scenes/discussion1?scene';
-import discussion2 from './scenes/discussion2?scene';
-import discussion3 from './scenes/discussion3?scene';
+import discussion_1 from './scenes/discussion_1?scene';
+import discussion_2 from './scenes/discussion_2?scene';
+import discussion_3 from './scenes/discussion_3?scene';
 import graph_coloring from './scenes/graph_coloring?scene';
 import np_to_coloring_algorithm from './scenes/np_to_coloring_algorithm?scene';
 import np_to_coloring_big_graph from './scenes/np_to_coloring_big_graph?scene';
@@ -27,9 +27,9 @@ export default makeProject({
     graph_coloring,
     building_the_protocol_1,
     building_the_protocol_2,
-    discussion1,
-    discussion2,
-    discussion3,
+    discussion_1,
+    discussion_2,
+    discussion_3,
     commitments_1,
     commitments_2,
     sudoku_reduction,
