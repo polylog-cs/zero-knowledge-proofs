c2bbc1d5aa7471100942bcec931b43c960e66487
Václav Volhejn <vaclav.volhejn@gmail.com>
2025-01-29 10:07:45 +0100

    Make scene names consistent with script

--- src/project.ts
+++ src/project.ts
@@ -1,13 +1,13 @@
 import { makeProject } from '@motion-canvas/core';
 
-import building_protocol_1 from './scenes/building_protocol_1?scene';
-import building_protocol_2 from './scenes/building_protocol_2?scene';
-import coloring from './scenes/coloring?scene';
+import building_the_protocol_1 from './scenes/building_the_protocol_1?scene';
+import building_the_protocol_2 from './scenes/building_the_protocol_2?scene';
 import commitments_1 from './scenes/commitments_1?scene';
 import commitments_2 from './scenes/commitments_2?scene';
 import discussion1 from './scenes/discussion1?scene';
 import discussion2 from './scenes/discussion2?scene';
 import discussion3 from './scenes/discussion3?scene';
+import graph_coloring from './scenes/graph_coloring?scene';
 import np_to_coloring_algorithm from './scenes/np_to_coloring_algorithm?scene';
 import np_to_coloring_big_graph from './scenes/np_to_coloring_big_graph?scene';
 import np_to_coloring_conversion from './scenes/np_to_coloring_conversion?scene';
@@ -24,9 +24,9 @@ export default makeProject({
   scenes: [
     teacher_failing,
     teacher,
-    coloring,
-    building_protocol_1,
-    building_protocol_2,
+    graph_coloring,
+    building_the_protocol_1,
+    building_the_protocol_2,
     discussion1,
     discussion2,
     discussion3,
