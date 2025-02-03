8ea3ae9ef01a079fda0ff6b2590710e1ab64f3bc
Richard Hladík <rihl@uralyx.cz>
2025-02-03 17:54:01 +0100

    Shorten algorithm steps

--- src/scenes/building_the_protocol_2.tsx
+++ src/scenes/building_the_protocol_2.tsx
@@ -1,5 +1,5 @@
 import { makeScene2D } from '@motion-canvas/2d';
-import { all, useLogger, waitFor } from '@motion-canvas/core';
+import { all, delay, useLogger, waitFor } from '@motion-canvas/core';
 
 import { Solarized } from '../utilities';
 import { exampleGraphData } from '../utilities_graph';
@@ -13,27 +13,34 @@ export default makeScene2D(function* (view) {
 
   yield* scene.setup('prover');
 
-  yield* all(
-    scene.addText('prover', '1. Lock the colors'),
-    scene.graphRef().lockVertices(),
-  );
+  yield* all(scene.addText('prover', '1. Lock'), scene.graphRef().lockVertices());
 
   yield* scene.sendGraph('verifier');
 
   const challengeEdge: [string, string] = exampleGraphData.edges[0];
   yield* all(
-    scene.addText('verifier', '2. Challenge an edge'),
+    scene.addText('verifier', '2. Challenge'),
     scene.graphRef().pointAtEdge(challengeEdge, true, 2, false),
   );
 
   yield* all(
-    scene.addText('prover', '3. Reveal the colors'),
+    scene.addText('prover', '3. Reveal'),
     scene.graphRef().unlockVertices(challengeEdge),
     scene.graphRef().removeArrows(),
   );
 
-  yield* all(scene.addText('verifier', '4. Check the colors'));
-  scene.verifierRef().expression('thinking');
+  yield* all(
+    scene.addText('verifier', '4. Check'),
+    delay(
+      1,
+      (function* () {
+        scene.verifierRef().expression('thinking');
+      })(),
+    ),
+  );
+
+  yield* waitFor(1);
+  scene.verifierRef().expression('happy');
 
   yield* waitFor(5);
 });
