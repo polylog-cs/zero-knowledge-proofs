d0af64812d2b1df252f0d4dc86a1b48f7324540c
Richard Hladík <rihl@uralyx.cz>
2025-02-03 17:55:04 +0100

    Comment out face expressions that I think are out of place

--- src/scenes/discussion_2.tsx
+++ src/scenes/discussion_2.tsx
@@ -146,8 +146,9 @@ export default makeScene2D(function* (view) {
           1,
         ),
       );
-      scene.verifierRef().expression('alarmed');
-      scene.proverRef().expression('happy');
+      // RH: tohle sem podle mě nesedí, Vaškův face je spíš jako "are you kidding me?"
+      // scene.verifierRef().expression('alarmed');
+      // scene.proverRef().expression('happy');
       yield* all(
         cross().scale(5, 1),
         cross().opacity(1, 1),
